b423d203d8eeea64c6dee79c112f6ac215cb6556
Pawel-Raciborski <wojak2845@gmail.com>
2024-08-04 21:39:13 +0200

    - getting DOM elements - creating btnRoll event listener

--- pig-game-project/starter/script.js
+++ pig-game-project/starter/script.js
@@ -1,10 +1,36 @@
 'use strict';
 
+let currentScore = 0;
+
 // Selecting elements
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceElement = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
+
+  //check for rolled 1
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // TODO change player ture
+  } else {
+  }
+});
