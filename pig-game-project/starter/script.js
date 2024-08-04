78783d11e7279e7324fbf720d8e4d82ef71baa58
Pawel-Raciborski <wojak2845@gmail.com>
2024-08-04 21:55:28 +0200

    - player switch logic created

--- pig-game-project/starter/script.js
+++ pig-game-project/starter/script.js
@@ -1,8 +1,13 @@
 'use strict';
 
 let currentScore = 0;
+let activePlayer = 0;
+
+const scores = [0, 0];
 
 // Selecting elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -30,7 +35,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // TODO change player ture
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore; // TODO change player ture
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
