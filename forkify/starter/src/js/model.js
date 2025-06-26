8bfab70054dbd4456ee3124e6c2c83cb0355eba0
Pawel-Raciborski <wojak2845@gmail.com>
2025-06-26 18:55:01 +0200

    Error message added

---
+++ forkify/starter/src/js/model.js
@@ -0,0 +1,28 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+import recipeView from './views/recipeView.js';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+    console.log(`loadRecipe data:`, data);
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.error(`${err} 💣💣`);
+    throw err;
+  }
+};
