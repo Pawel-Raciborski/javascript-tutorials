a9c3d4c2ff837395e0bb90356c9fc7615436ffc9
Pawel-Raciborski <wojak2845@gmail.com>
2025-06-30 14:58:28 +0200

    Implementacja paginacji cz. 1

--- forkify/starter/src/js/model.js
+++ forkify/starter/src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config.js';
+import { API_URL, RESULTS_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 import recipeView from './views/recipeView.js';
 
@@ -7,6 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RESULTS_PER_PAGE,
   },
 };
 
@@ -49,4 +51,10 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-loadSearchResults('pizza');
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+};
